b64c8785b13f217d8048f35d0350a1c2c9dd8174
Nikhil Lele <nik.lele@gmail.com>
2017-04-15 13:50:46 -0700

    move stuff to 1 file

--- bot.rb
+++ bot.rb
@@ -6,5 +6,28 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
  
 Bot.on :message do |message|
+    # parse(message.text)
     message.reply(text: 'Hello!')
+end
+
+# parse eg 1d12+3
+def parse(msg)
+    rollPattern = /roll (?<n>\d+)d(?<f>\d+)\+?(?<c>\d+)?/i
+    matches = rollPattern.match(msg)
+    if matches.nil?
+        return nil
+    end
+    n = matches['n']
+    f = matches['f']
+    c = matches['c'].nil? ? 0 : matches['c']
+    return n, f, c
+end
+
+# roll n dice with f faces each, and add c at the end
+def roll(n, f, c)
+    s = 0
+    (0..n).each do
+        s += rand(f) + 1
+    end
+    return s + c
 end
